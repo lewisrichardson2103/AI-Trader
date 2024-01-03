27fda05f5447bf252c1ae2dc1f4dff20e8217da3
lewisrichardson2103 <lewisrichardson2103@users.noreply.github.com>
2024-01-03 13:45:50 +0000

    Removed things not related to trading. Updated the enviroment and agent as well as added a back test during training

--- environment.py
+++ environment.py
@@ -1,135 +1,184 @@
+from sklearn.preprocessing import Normalizer
+from ta.trend import SMAIndicator
+from ta.volatility import AverageTrueRange
+from ta.momentum import RSIIndicator
+from ta.utils import dropna
 import random
 import numpy as np
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 
 
 class Environment:
-    def __init__(self, grid_size, render_on=False):
-        self.grid_size = grid_size
-        self.grid = []
+    def __init__(self, file_path, window_size=20, max_steps=200, capital=1000, capital_goal=1010, render_on=False):
+        self.window_size = window_size
         self.render_on = render_on
-        self.agent_location = None
-        self.goal_location = None
+        self.max_steps = max_steps
+        self.capital_goal = capital_goal
+        self.current_step = 0
+        self.total_reward = 0
+
+        self.starting_capital = capital
+        self.capital = 0
+        self.profit = 0
+        # Raw data not normalised, additional columns added
+        self.raw_data = self.load_data(file_path)
+        self.normaliser = Normalizer('l2')
+        self.reset()
+        print(self.data)
 
     def reset(self):
-        # Initialize the empty grid as a 2d array of 0s
-        self.grid = np.zeros((self.grid_size, self.grid_size))
-
-        # Add the agent and the goal to the grid
-        self.agent_location = self.add_agent()
-        self.goal_location = self.add_goal()
+        self.capital = self.starting_capital
+        self.profit = 0
+        self.positions = []
+        self.current_step = 0
+        # self.total_reward = 0
+        if self.max_steps == len(self.raw_data):
+            self.data = self.raw_data
+        else:
+            start = random.randint(0, (len(self.raw_data) - (self.max_steps)))
+            self.data = self.raw_data.iloc[start: start + (self.max_steps)]
+        self.state = None
 
         if self.render_on:
             self.render()
 
         # Return the initial state of the grid
-        return self.get_state()
+        return self.get_state(0)
 
-    def add_agent(self):
-        # Choose a random location
-        location = (random.randint(0, self.grid_size - 1),
-                    random.randint(0, self.grid_size - 1))
+    def load_data(self, file_path):
+        # Load the raw data from csv
+        data = pd.read_csv(file_path, index_col=0)
+        data.index = pd.to_datetime(data.index, dayfirst=True)
+        data['time_of_day'] = data.index
+        data['time_of_day'] = (
+            data['time_of_day'] - data['time_of_day'].dt.normalize()) / pd.Timedelta(seconds=1)  # Seconds since midnight
+        data = data.dropna(axis=1)
 
-        # Agent is represented by a 1
-        self.grid[location[0]][location[1]] = 1
+        # Add indicators here
 
-        return location
+        # Add state columns here
+        data['num_positions'] = 0
+        data['position_value'] = 0.0
+        data['capital'] = 0.0
+        data['profit'] = 0.0
+        data['action'] = 0
 
-    def add_goal(self):
-        # Choose a random location
-        location = (random.randint(0, self.grid_size - 1),
-                    random.randint(0, self.grid_size - 1))
-
-        # Get a random location until it is not occupied
-        while self.grid[location[0]][location[1]] == 1:
-            location = (random.randint(0, self.grid_size - 1),
-                        random.randint(0, self.grid_size - 1))
-
-        # Goal is represented by a -1
-        self.grid[location[0]][location[1]] = -1
-
-        return location
+        return data
 
     def render(self):
-        # Convert to a list of ints to improve formatting
-        grid = self.grid.astype(int).tolist()
+        # print(f'Capital: {self.capital}')
+        # print(f'Profit: {self.profit}')
+        # print(f'Total Reward for episode: {self.total_reward}')
+        print('TODO Render')
+        print()
 
-        for row in grid:
-            print(row)
-        print('')  # To add some space between renders for each step
+    def get_state(self, action):
+        starting_id = (self.current_step - self.window_size) + 1
 
-    def get_state(self):
-        # Flatten the grid from 2d to 1d
-        state = self.grid.flatten()
-        return state
+        self.data['num_positions'].iloc[self.current_step] = len(
+            self.positions)
+        self.data['position_value'].iloc[self.current_step] = len(
+            self.positions) * self.data['close'].iloc[self.current_step]
+        self.data['capital'].iloc[self.current_step] = self.capital
+        self.data['profit'].iloc[self.current_step] = self.profit
+        self.data['action'].iloc[self.current_step] = action
 
-    def move_agent(self, action):
-        # Map agent action to the correct movement
-        moves = {
-            0: (-1, 0),  # Up
-            1: (1, 0),  # Down
-            2: (0, -1),  # Left
-            3: (0, 1)   # Right
-        }
+        if starting_id >= 0:
+            windowed_data = self.data.iloc[starting_id:self.current_step+1]
+        else:
+            windowed_data = self.data.iloc[0]
+            windowed_data = [windowed_data.copy()
+                             for _ in range(abs(starting_id))]
+            for i in range(0, self.current_step + 1):
+                windowed_data.append(self.data.iloc[i])
 
-        previous_location = self.agent_location
+            windowed_data = pd.DataFrame(windowed_data)
+            # windowed_data = windowed_data.set_index('datetime')
 
-        # Determine the new location after applying the action
-        move = moves[action]
-        new_location = (previous_location[0] +
-                        move[0], previous_location[1] + move[1])
+        values = windowed_data.values
+        state = self.normaliser.fit_transform(values)
+        return np.array([state])
 
-        done = False  # The episode is not done by default
-        reward = 0   # Initialize reward
+    def perform_action(self, action):
+        done = True if self.current_step >= self.max_steps else False
+        buy_reward = 0
+        sell_reward = 0
+        hold_reward = 0
+        closed_market_reward = 0
 
         # Check for a valid move
-        if self.is_valid_location(new_location):
-            # Remove agent from old location
-            self.grid[previous_location[0]][previous_location[1]] = 0
+        if self.is_valid_trade(action):
+            reward = 2
+            if action == 0:
+                hold_reward = 0.01
+            elif action == 1:  # Buy
+                price = self.data['close'].iloc[self.current_step]
+                self.positions.append(price)
+                self.capital -= price
+                buy_reward += (self.data['close'].iloc[self.current_step] -
+                               self.data['close'].iloc[self.current_step - 1])  # Reward for buying when the stock is falling
+                if buy_reward < -1:
+                    buy_reward = -1
+            elif action == 2:  # Sell
+                buy_price = self.positions.pop()
+                profit = self.data['close'].iloc[self.current_step] - buy_price
+                self.profit += profit
+                self.capital += self.data['close'].iloc[self.current_step]
+                sell_reward += profit  # Reward for positive profit
+                if sell_reward < -1:
+                    sell_reward = -1
 
-            # Add agent to new location
-            self.grid[new_location[0]][new_location[1]] = 1
+            # We now penalise for having open positions over a certain time (day trading)
+            # This is after 14:00 (market closes at 16:00)
+            # if self.data['time_of_day'].iloc[self.current_step] >= 50400 and (action == 0 or action == 1):
+            #     # Number of hours over
+            #     delta = (
+            #         self.data['time_of_day'].iloc[self.current_step] - 50400) // 3600
+            #     closed_market_reward = (delta * len(self.positions) * 0.01)
 
-            # Update agent's location
-            self.agent_location = new_location
+            # Reward for moving towards more capital
+            goal_reward = (self.capital - self.capital_goal) * 0.01
 
-            # Check if the new location is the reward location
-            if self.agent_location == self.goal_location:
-                # Reward for getting the goal
-                reward = 100
+            reward += (buy_reward + sell_reward + hold_reward +
+                       closed_market_reward + goal_reward)
 
-                # Episode is complete
-                done = True
-            else:
-                # Calculate the distance before the move
-                previous_distance = np.abs(self.goal_location[0] - previous_location[0]) + \
-                    np.abs(self.goal_location[1] - previous_location[1])
-
-                # Calculate the distance after the move
-                new_distance = np.abs(self.goal_location[0] - new_location[0]) + \
-                    np.abs(self.goal_location[1] - new_location[1])
-
-                # If new_location is closer to the goal, reward = 1, if further, reward = -1
-                reward = (previous_distance - new_distance) - 0.1
+            # Ensure no negative reward when we used a valid trade
+            if reward < 0:
+                reward = 0
         else:
             # Slightly larger punishment for an invalid move
-            reward = -3
+            reward = -1
 
         return reward, done
 
-    def is_valid_location(self, location):
-        # Check if the location is within the boundaries of the grid
-        if (0 <= location[0] < self.grid_size) and (0 <= location[1] < self.grid_size):
+    def is_valid_trade(self, action):
+        if action == 0:  # Hold
             return True
+        elif action == 1:  # Buy
+            if self.capital >= self.data['close'].iloc[self.current_step]:
+                return True
+            else:
+                return False
+        elif action == 2:  # Sell
+            if len(self.positions) > 0:
+                return True
+            else:
+                return False
         else:
             return False
 
-    def step(self, action):
+    def step(self, action, step):
         # Apply the action to the environment, record the observations
-        reward, done = self.move_agent(action)
-        next_state = self.get_state()
+        self.current_step = step
+        reward, done = self.perform_action(action)
+        self.total_reward += reward
+        next_state = self.get_state(action)
+        if self.current_step == self.max_steps - 1:
+            done = True
 
         # Render the grid at each step
-        if self.render_on:
+        if self.render_on and self.current_step == self.max_steps - 1:
             self.render()
 
         return reward, next_state, done
--- main.py
+++ main.py
@@ -1,38 +1,94 @@
 from environment import Environment
 from agent import Agent
 from experience_replay import ExperienceReplay
+import matplotlib.pyplot as plt
 import time
+import numpy as np
+
+
+def backtest():
+    window_size = 20
+
+    environment = Environment('test_data/Validation_Data.csv', window_size=window_size,
+                              max_steps=180, capital=1000, capital_goal=1010, render_on=True)
+    environment.max_steps = len(environment.raw_data)
+    state = environment.reset()
+
+    features = len(environment.data.columns)
+    agent = Agent(feature_size=features, window_size=window_size,
+                  epsilon=0.01, epsilon_decay=0.9998, epsilon_end=0.01)
+    agent.load(f'models/model_{features}.keras')
+
+    # Loop until the dataset is done
+    for step in range(environment.max_steps):
+        # print('Step:', step)
+        # Get the action choice from the agents policy
+        action = agent.get_action(state)
+
+        # Take a step in the environment and save the experience
+        _, next_state, done = environment.step(action, step)
+
+        # Set the state to the next_state
+        state = next_state
+
+    print(
+        f'Finished at step: {step}')
+    print(f'Capital: {environment.capital}')
+    print('Profit from trades:', environment.profit)
+    print(
+        f'Position Value: {environment.data.position_value.iloc[-1]}')
+    print(
+        f'Total Equity: {( environment.capital + environment.data.position_value.iloc[-1])}')
+
+    plt_data = environment.data.iloc[:180]
+    plt_data['equity'] = environment.data.capital + \
+        environment.data.position_value
+    plt_data['buy'] = np.where(plt_data.action == 1, plt_data.close, np.nan)
+    plt_data['sell'] = np.where(plt_data.action == 2, plt_data.close, np.nan)
+
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Vertically stacked subplots')
+    axs[0].plot(plt_data.index, plt_data.close)
+    axs[0].scatter(plt_data.index, plt_data.buy, marker="x", color='g')
+    axs[0].scatter(plt_data.index, plt_data.sell, marker="x", color='r')
+    axs[1].plot(plt_data.index, plt_data.equity)
+    plt.show()
+
 
 if __name__ == '__main__':
 
-    grid_size = 8
+    backtesting = True
 
-    environment = Environment(grid_size=grid_size, render_on=True)
-    agent = Agent(grid_size=grid_size, epsilon=1, epsilon_decay=0.9998, epsilon_end=0.01)
+    window_size = 20
+    max_steps = 180
+    rewards = []
+
+    environment = Environment('test_data/AAPL_Daily_2018_2019.csv', window_size=window_size,
+                              max_steps=max_steps, capital=1000, capital_goal=1010, render_on=True)
+    features = len(environment.data.columns)
+    agent = Agent(feature_size=features, window_size=window_size,
+                  epsilon=1.0, epsilon_decay=0.9998, epsilon_end=0.01, learning_rate=0.0001)
+    # agent.load(f'models/model_{features}.keras')
     experience_replay = ExperienceReplay(capacity=10000, batch_size=32)
-    # agent.load(f'models/model_{grid_size}.h5')
-    
+
     # Number of episodes to run before training stops
-    episodes = 5000
-    # Max number of steps in each episode
-    max_steps = 250
+    episodes = 100
 
     for episode in range(episodes):
         # Get the initial state of the environment and set done to False
         state = environment.reset()
-
+        print('Episode:', episode)
         # Loop until the episode finishes
         for step in range(max_steps):
-            print('Episode:', episode)
-            print('Step:', step)
-            print('Epsilon:', agent.epsilon)
+            # print('Step:', step)
 
             # Get the action choice from the agents policy
             action = agent.get_action(state)
 
             # Take a step in the environment and save the experience
-            reward, next_state, done = environment.step(action)
-            experience_replay.add_experience(state, action, reward, next_state, done)
+            reward, next_state, done = environment.step(action, step)
+            experience_replay.add_experience(
+                state, action, reward, next_state, done)
 
             # If the experience replay has enough memory to provide a sample, train the agent
             if experience_replay.can_provide_sample():
@@ -43,9 +99,27 @@ if __name__ == '__main__':
             state = next_state
 
             if done:
+                print(
+                    f'Finished at step: {step}. Time range was: {environment.data.time_of_day.iloc[0]} to {environment.data.time_of_day.iloc[-1]}')
+                print(f'Capital: {environment.capital}')
+                print('Profit from trades:', environment.profit)
+                print(
+                    f'Position Value: {environment.data.position_value.iloc[-1]}')
+                print(
+                    f'Total Equity: {( environment.capital + environment.data.position_value.iloc[-1])}')
+                print('Average Reward:',
+                      environment.total_reward / (episode + 1))
+                print('Epsilon:', agent.epsilon)
+                rewards.append(environment.total_reward / (episode + 1))
                 break
 
             # Optionally, pause for half a second to evaluate the model
             # time.sleep(0.5)
-    
-        agent.save(f'models/model_{grid_size}.h5')
+        if environment.profit > 0:
+            agent.save(f'models/model_{features}.keras')
+            print('Model Saved')
+        if episode % 10 == 0 and episode != 0:
+            plt.plot(rewards)
+            plt.show()
+            if backtesting:
+                backtest()
